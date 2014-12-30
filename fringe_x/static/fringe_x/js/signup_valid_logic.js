7712ffbf5f5421d7229390687d5eab726b2caf36
daemon.sud <absoluteai.sud@gmail.com>
2014-12-31 01:03:54 +0530

    applied check email notification on signup page of fringe_circle

--- fringe_x/static/fringe_x/js/signup_valid_logic.js
+++ fringe_x/static/fringe_x/js/signup_valid_logic.js
@@ -14,7 +14,21 @@ else if(altEmailElement==workEmailElement)
 alert("work and alternate email cannot be same");
 }
 else{
-$("#signup_form").submit();
+$( "#signup-confirm" ).dialog({
+      resizable: false,
+      height:300,
+      modal: true,
+      buttons: {
+        "OK, Got it!": function() {
+          //$( this ).dialog( "close" );
+          $("#signup_form").submit();
+        },
+        "Cancel Signup": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+//$("#signup_form").submit();
 }
 //alert('hello');
 }
