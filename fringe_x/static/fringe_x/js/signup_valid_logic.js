c3bd8d7b6b093078558ce4598cca97c00553d3b6
daemon.sud <absoluteai.sud@gmail.com>
2014-12-30 20:02:32 +0530

    created signup_valid_logic.js to handle validation in new signup form, validated all fields in new signup form, made changes in css file of signup form

---
+++ fringe_x/static/fringe_x/js/signup_valid_logic.js
@@ -0,0 +1,25 @@
+function validate_input()
+{
+var altEmailElement=$("#altEmail").val();
+var workEmailElement=$("#workEmail").val();
+var termsConditionsCheckBox=document.getElementById("conditionCheckBox");
+if($(termsConditionsCheckBox).is(':checked'))
+{
+if(altEmailElement=="" || workEmailElement=="")
+{
+alert("Provide Both email Addresses");
+}
+else if(altEmailElement==workEmailElement)
+{
+alert("work and alternate email cannot be same");
+}
+else{
+$("#signup_form").submit();
+}
+//alert('hello');
+}
+else
+{
+alert("Please check and agree with the terms and conditions!!");
+}
+}
